c32e4d1407ee6afb8d62ab9679b17684344b3964
Anjan Roy <anjanroy@yandex.com>
2021-05-12 08:26:10 +0530

    subscriber

---
+++ subscriber/subscriber.go
@@ -0,0 +1,60 @@
+package subscriber
+
+import (
+	"net"
+	"sync"
+)
+
+// Subscriber - Abstraction layer at which subscriber interacts, while
+// all underlying networking details are kept hidden
+type Subscriber struct {
+	Id        uint64
+	Conn      *net.Conn
+	topicLock *sync.RWMutex
+	Topics    map[string]bool
+}
+
+// AddSubscription - After a subscriber has been created, more topics
+// can be subscribed to
+func (s *Subscriber) AddSubscription(topics ...string) uint64 {
+	if len(topics) == 0 {
+		return 0
+	}
+
+	s.topicLock.Lock()
+	defer s.topicLock.Unlock()
+
+	var subCount uint64
+
+	for i := 0; i < len(topics); i++ {
+		if _, ok := s.Topics[topics[i]]; ok {
+			continue
+		}
+		s.Topics[topics[i]] = true
+		subCount++
+	}
+
+	return subCount
+}
+
+// Unsubscribe - Unsubscribe from a non-empty set of topics
+func (s *Subscriber) Unsubscribe(topics ...string) uint64 {
+	if len(topics) == 0 {
+		return 0
+	}
+
+	s.topicLock.Lock()
+	defer s.topicLock.Unlock()
+
+	var unsubCount uint64
+
+	for i := 0; i < len(topics); i++ {
+		if _, ok := s.Topics[topics[i]]; !ok {
+			continue
+		}
+		delete(s.Topics, topics[i])
+		unsubCount++
+	}
+
+	return unsubCount
+}
