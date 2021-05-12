c77eb5b3519f7322e35d1dac72df614efc1f43ad
Anjan Roy <anjanroy@yandex.com>
2021-05-12 09:04:33 +0530

    publisher

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -2,6 +2,7 @@ package publisher
 
 import (
 	"context"
+	"log"
 	"net"
 
 	"github.com/itzmeanjan/pubsub"
@@ -9,11 +10,39 @@ import (
 
 // Publisher - Abstraction layer at which publisher operates
 type Publisher struct {
-	Conn net.Conn
+	conn    net.Conn
+	msgChan chan *msgPublishRequest
+}
+
+// msgPublishRequest - Message publish request to be received
+// in this form
+type msgPublishRequest struct {
+	msg     *pubsub.Message
+	resChan chan uint64
+}
+
+// start - Lifecycle manager of publisher, accepts publish request
+// acts on it & responds back
+func (p *Publisher) start(ctx context.Context, running chan struct{}) {
+	close(running)
+
+	for {
+		select {
+		case <-ctx.Done():
+			if err := p.conn.Close(); err != nil {
+				log.Printf("[pub0sub] Error : %s\n", err.Error())
+			}
+			return
+
+		case req := <-p.msgChan:
+			req.resChan <- 0
+
+		}
+	}
 }
 
 // New - New publisher instance, attempts to establish connection with remote
-// & returns handle with open connection
+// & returns handle with open connection, ready for use
 func New(ctx context.Context, proto, addr string) (*Publisher, error) {
 	var d net.Dialer
 
@@ -22,17 +51,19 @@ func New(ctx context.Context, proto, addr string) (*Publisher, error) {
 		return nil, err
 	}
 
-	return &Publisher{Conn: conn}, nil
+	pub := &Publisher{conn: conn, msgChan: make(chan *msgPublishRequest, 1)}
+	running := make(chan struct{})
+	go pub.start(ctx, running)
+	<-running
+
+	return pub, nil
 }
 
 // Publish - Sends message publish request over network,
 // returns back how many of subscribers received this message
 func (p *Publisher) Publish(msg *pubsub.Message) uint64 {
-	return 0
-}
+	resChan := make(chan uint64, 1)
+	p.msgChan <- &msgPublishRequest{msg: msg, resChan: resChan}
 
-// Close - Publisher is supposed to invoke this when
-// it's done with its publishing interest
-func (p *Publisher) Close() error {
-	return p.Conn.Close()
+	return <-resChan
 }
