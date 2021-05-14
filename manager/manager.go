252edae0d33b1a732c6a317e2e4bec0c96f5bb1d
Anjan Roy <anjanroy@yandex.com>
2021-05-14 21:45:46 +0530

    new manager instance

--- manager/manager.go
+++ manager/manager.go
@@ -2,6 +2,8 @@ package manager
 
 import (
 	"context"
+	"log"
+	"net"
 	"sync"
 	"time"
 
@@ -19,8 +21,30 @@ type Manager struct {
 	subscribers map[string]map[uint64]*subscriber.Subscriber
 }
 
-// start - Flow of manager during its lifetime
-func (m *Manager) start(ctx context.Context, running chan struct{}) {
+// New - One new manager instance, ready to be used, with all components running
+func New(ctx context.Context, addr string, ping chan struct{}, hub *hub.Hub) *Manager {
+	manager := Manager{
+		ping:        ping,
+		hub:         hub,
+		subLock:     &sync.RWMutex{},
+		subscribers: make(map[string]map[uint64]*subscriber.Subscriber),
+	}
+
+	done := make(chan bool)
+	manager.listen(ctx, addr, hub, done)
+	if !<-done {
+		return nil
+	}
+
+	running := make(chan struct{})
+	manager.process(ctx, running)
+	<-running
+
+	return &manager
+}
+
+// process - Processes message publish request from HUB
+func (m *Manager) process(ctx context.Context, running chan struct{}) {
 	close(running)
 
 	for {
@@ -86,4 +110,77 @@ func (m *Manager) start(ctx context.Context, running chan struct{}) {
 
 		}
 	}
+}
+
+// listen - Listens to TCP connection on specified <address:port>
+// accepts those & starts processing
+func (m *Manager) listen(ctx context.Context, addr string, hub *hub.Hub, done chan bool) {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Printf("[pub0sub] Error : %s\n", err.Error())
+
+		done <- false
+		return
+	}
+
+	defer func() {
+		if err := lis.Close(); err != nil {
+			log.Printf("[pub0sub] Error : %s\n", err.Error())
+		}
+	}()
+
+	done <- true
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		default:
+			conn, err := lis.Accept()
+			if err != nil {
+				log.Printf("[pub0sub] Error : %s\n", err.Error())
+				break
+			}
+
+			go handleTCPConnection(ctx, conn, hub)
+		}
+	}
+}
+
+// handleTCPConnection - Each publisher/ subscriber connection is handled
+// in its own go routine
+func handleTCPConnection(ctx context.Context, conn net.Conn, hub *hub.Hub) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("[pub0sub] Error : %s\n", err.Error())
+		}
+	}()
+
+STOP:
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		default:
+			op := new(ops.OP)
+			_, err := op.ReadFrom(conn)
+			if err != nil {
+				break STOP
+			}
+
+			switch *op {
+			case ops.PUB_REQ:
+			case ops.NEW_SUB_REQ:
+			case ops.MSG_PUSH:
+			case ops.ADD_SUB_REQ:
+			case ops.UNSUB_REQ:
+			case ops.UNSUPPORTED:
+				break STOP
+			}
+
+		}
+	}
+
 }
