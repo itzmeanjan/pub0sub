75181a1da9329fa7194424a11f76732d990cba30
Anjan Roy <anjanroy@yandex.com>
2021-05-17 22:25:26 +0530

    multiple topic publish/ subscribe

--- cli/publisher/0pub.go
+++ cli/publisher/0pub.go
@@ -5,21 +5,43 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/itzmeanjan/pub0sub/ops"
 	"github.com/itzmeanjan/pub0sub/publisher"
 )
 
+type topicList []string
+
+func (t *topicList) String() string {
+	if t == nil {
+		return ""
+	}
+
+	return strings.Join(*t, "")
+}
+
+func (t *topicList) Set(val string) error {
+	*t = append(*t, val)
+	return nil
+}
+
 func main() {
 	var (
-		proto = "tcp"
-		addr  = flag.String("addr", "127.0.0.1", "Connect to address")
-		port  = flag.Uint64("port", 13000, "Connect to port")
-		topic = flag.String("topic", "topic_1", "Topic to publish data on")
-		data  = flag.String("data", "hello", "Data to publish")
+		proto  = "tcp"
+		addr   = flag.String("addr", "127.0.0.1", "Connect to address")
+		port   = flag.Uint64("port", 13000, "Connect to port")
+		data   = flag.String("data", "hello", "Data to publish")
+		topics topicList
 	)
+	flag.Var(&topics, "topic", "Topic to publish data on")
 	flag.Parse()
+
+	if len(topics) == 0 {
+		log.Printf("[0pub] Error : no topics specified\n")
+		return
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	fullAddr := fmt.Sprintf("%s:%d", *addr, *port)
@@ -30,7 +52,7 @@ func main() {
 	}
 
 	log.Printf("[0pub] Connected to %s\n", fullAddr)
-	msg := ops.Msg{Topics: []string{*topic}, Data: []byte(*data)}
+	msg := ops.Msg{Topics: topics, Data: []byte(*data)}
 	log.Printf("[0pub] Approximate message reach %d\n", pub.Publish(&msg))
 
 	cancel()
--- cli/subscriber/0sub.go
+++ cli/subscriber/0sub.go
@@ -7,11 +7,27 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/itzmeanjan/pub0sub/subscriber"
 )
+
+type topicList []string
+
+func (t *topicList) String() string {
+	if t == nil {
+		return ""
+	}
+
+	return strings.Join(*t, "")
+}
+
+func (t *topicList) Set(val string) error {
+	*t = append(*t, val)
+	return nil
+}
 
 func main() {
 	var (
@@ -19,13 +35,19 @@ func main() {
 		addr     = flag.String("addr", "127.0.0.1", "Connect to address")
 		port     = flag.Uint64("port", 13000, "Connect to port")
 		capacity = flag.Uint64("capacity", 1024, "Pending message queue capacity")
-		topic    = flag.String("topic", "topic_1", "Topic to subscribe")
+		topics   topicList
 	)
+	flag.Var(&topics, "topic", "Topic to subscribe")
 	flag.Parse()
+
+	if len(topics) == 0 {
+		log.Printf("[0sub] Error : no topics specified\n")
+		return
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	fullAddr := fmt.Sprintf("%s:%d", *addr, *port)
-	sub, err := subscriber.New(ctx, proto, fullAddr, *capacity, *topic)
+	sub, err := subscriber.New(ctx, proto, fullAddr, *capacity, topics...)
 	if err != nil {
 		log.Printf("[0sub] Error : %s\n", err.Error())
 		return
