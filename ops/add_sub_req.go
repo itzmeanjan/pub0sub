f4fbd55fd3a176a9b05c857c3310e0fb0fc18d79
Anjan Roy <anjanroy@yandex.com>
2021-05-22 08:05:05 +0530

    wire format changed for add subscription request [async-io]

--- ops/add_sub_req.go
+++ ops/add_sub_req.go
@@ -12,6 +12,44 @@ type AddSubscriptionRequest struct {
 	Topics []string
 }
 
+// Total size of message to be written into stream
+// as message envelope, so that Hub can figure out
+// how large buffer should it allocate for reading
+// whole message
+func (a *AddSubscriptionRequest) size() uint32 {
+	var size uint32
+	size += 13 // ( --- <4B> + <8B> + <1B> --- )
+
+	for i := 0; i < len(a.Topics); i++ {
+		size += (1 + uint32(len(a.Topics[i])))
+	}
+
+	return size
+}
+
+// WriteEnvelope - Subscriber invokes for writing message envelope
+// so that Hub can understand `how to handle message ?`
+//
+// It also includes size of total message except first 1-byte opcode
+//
+// It should write 5-bytes into stream, in ideal condition
+func (a *AddSubscriptionRequest) WriteEnvelope(w io.Writer) (int64, error) {
+	var size int64
+
+	opCode := ADD_SUB_REQ
+	if _, err := opCode.WriteTo(w); err != nil {
+		return size, err
+	}
+
+	size += 1
+
+	if err := binary.Write(w, binary.BigEndian, a.size()); err != nil {
+		return size, err
+	}
+
+	return size + 4, nil
+}
+
 // WriteTo - Writes subscription request to stream in recoverable form
 func (a *AddSubscriptionRequest) WriteTo(w io.Writer) (int64, error) {
 	var size int64
@@ -23,19 +61,19 @@ func (a *AddSubscriptionRequest) WriteTo(w io.Writer) (int64, error) {
 	size += 8
 
 	lTopics := len(a.Topics)
-	if err := binary.Write(w, binary.BigEndian, uint32(lTopics)); err != nil {
+	if err := binary.Write(w, binary.BigEndian, uint8(lTopics)); err != nil {
 		return size, err
 	}
 
-	size += 4
+	size += 1
 
 	for i := 0; i < lTopics; i++ {
 		lTopic := len(a.Topics[i])
-		if err := binary.Write(w, binary.BigEndian, uint32(lTopic)); err != nil {
+		if err := binary.Write(w, binary.BigEndian, uint8(lTopic)); err != nil {
 			return size, err
 		}
 
-		size += 4
+		size += 1
 
 		if n, err := w.Write([]byte(a.Topics[i])); n != lTopic {
 			return size, err
@@ -57,21 +95,21 @@ func (a *AddSubscriptionRequest) ReadFrom(r io.Reader) (int64, error) {
 	}
 
 	size += 8
-	var lTopics uint32
+	var lTopics uint8
 	if err := binary.Read(r, binary.BigEndian, &lTopics); err != nil {
 		return size, err
 	}
 
-	size += 4
+	size += 1
 	topics := make([]string, 0, lTopics)
 
 	for i := 0; i < int(lTopics); i++ {
-		var lTopic uint32
+		var lTopic uint8
 		if err := binary.Read(r, binary.BigEndian, &lTopic); err != nil {
 			return size, err
 		}
 
-		size += 4
+		size += 1
 
 		topic := make([]byte, lTopic)
 		if n, err := r.Read(topic); n != int(lTopic) {
