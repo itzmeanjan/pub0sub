bff24db47ab6532067b152a9ac30e8b7455cb329
Anjan Roy <anjanroy@yandex.com>
2021-05-22 18:01:29 +0530

    not-required code removed; start up code in shape

--- hub/hub.go
+++ hub/hub.go
@@ -42,30 +42,45 @@ type Hub struct {
 
 // New - Creates a new instance of hub, ready to be used
 func New(ctx context.Context, addr string, cap uint64) (*Hub, error) {
-	hub := Hub{
-		subLock:        &sync.RWMutex{},
-		subscribers:    make(map[string]map[uint64]net.Conn),
-		revLock:        &sync.RWMutex{},
-		revSubscribers: make(map[uint64]map[string]bool),
-		queueLock:      &sync.RWMutex{},
-		pendingQueue:   make([]*ops.Msg, 0, cap),
-		ping:           make(chan struct{}, cap),
-		evict:          make(chan uint64, cap),
+	watcher, err := gaio.NewWatcher()
+	if err != nil {
+		return nil, err
 	}
 
-	done := make(chan bool)
-	go hub.Listen(ctx, addr, done)
-	if !<-done {
-		return nil, errors.New("failed to start listener")
+	hub := Hub{
+		watcher:                    watcher,
+		pendingPublishers:          make(map[net.Conn]bool),
+		pendingNewSubscribers:      make(map[net.Conn]bool),
+		pendingExistingSubscribers: make(map[net.Conn]bool),
+		pendingUnsubscribers:       make(map[net.Conn]bool),
+		enqueuedRead:               make(map[net.Conn]*enqueuedRead),
+		index:                      0,
+		subLock:                    &sync.RWMutex{},
+		subscribers:                make(map[string]map[uint64]net.Conn),
+		revLock:                    &sync.RWMutex{},
+		revSubscribers:             make(map[uint64]map[string]bool),
+		queueLock:                  &sync.RWMutex{},
+		pendingQueue:               make([]*ops.Msg, 0, cap),
+		ping:                       make(chan struct{}, cap),
+		evict:                      make(chan uint64, cap),
 	}
 
 	var (
-		runProc  = make(chan struct{})
-		runEvict = make(chan struct{})
+		runListener = make(chan bool)
+		runWatcher  = make(chan struct{})
+		runProc     = make(chan struct{})
+		runEvict    = make(chan struct{})
 	)
 
-	go hub.Process(ctx, runProc)
-	go hub.Evict(ctx, runEvict)
+	go hub.listen(ctx, addr, runListener)
+	if !<-runListener {
+		return nil, errors.New("failed to start listener")
+	}
+
+	go hub.watch(ctx, runWatcher)
+	go hub.process(ctx, runProc)
+	go hub.evictSubscribers(ctx, runEvict)
+	<-runWatcher
 	<-runProc
 	<-runEvict
 
@@ -162,7 +177,7 @@ func (h *Hub) handleRead(ctx context.Context, result gaio.OpResult) error {
 				return err
 			}
 
-			subCount := h.Publish(msg)
+			subCount := h.publish(msg)
 			oStream := new(bytes.Buffer)
 
 			rOp := ops.PUB_RESP
@@ -189,7 +204,7 @@ func (h *Hub) handleRead(ctx context.Context, result gaio.OpResult) error {
 				return err
 			}
 
-			subId, topicCount := h.Subscribe(result.Conn, msg.Topics...)
+			subId, topicCount := h.subscribe(result.Conn, msg.Topics...)
 			oStream := new(bytes.Buffer)
 
 			rOp := ops.NEW_SUB_RESP
@@ -216,7 +231,7 @@ func (h *Hub) handleRead(ctx context.Context, result gaio.OpResult) error {
 				return err
 			}
 
-			topicCount := h.AddSubscription(msg.Id, result.Conn, msg.Topics...)
+			topicCount := h.addSubscription(msg.Id, result.Conn, msg.Topics...)
 			oStream := new(bytes.Buffer)
 
 			rOp := ops.ADD_SUB_RESP
@@ -243,7 +258,7 @@ func (h *Hub) handleRead(ctx context.Context, result gaio.OpResult) error {
 				return err
 			}
 
-			topicCount := h.Unsubscribe(msg.Id, msg.Topics...)
+			topicCount := h.unsubscribe(msg.Id, msg.Topics...)
 			oStream := new(bytes.Buffer)
 
 			rOp := ops.UNSUB_RESP
@@ -313,9 +328,9 @@ func (h *Hub) handleWrite(ctx context.Context, result gaio.OpResult) error {
 	return nil
 }
 
-// publish - Actually writes message, along with opcode
+// Actually writes message, along with opcode
 // to network connection
-func (h *Hub) publish(op *ops.OP, msg *ops.Msg) {
+func (h *Hub) writeMessage(ctx context.Context, op *ops.OP, msg *ops.Msg) {
 	h.subLock.RLock()
 	defer h.subLock.RUnlock()
 
@@ -327,11 +342,18 @@ func (h *Hub) publish(op *ops.OP, msg *ops.Msg) {
 		}
 
 		pushMsg.Topic = msg.Topics[i]
+
+		buf := new(bytes.Buffer)
+		if _, err := op.WriteTo(buf); err != nil {
+			continue
+		}
+		if _, err := pushMsg.WriteTo(buf); err != nil {
+			continue
+		}
+
 		for _, conn := range subs {
-			if _, err := op.WriteTo(conn); err != nil {
-				continue
-			}
-			if _, err := pushMsg.WriteTo(conn); err != nil {
+			if err := h.watcher.Write(ctx, conn, buf.Bytes()); err != nil {
+				log.Printf("[pub0sub] Error : %s\n", err.Error())
 				continue
 			}
 		}
@@ -340,7 +362,7 @@ func (h *Hub) publish(op *ops.OP, msg *ops.Msg) {
 
 // Process - Listens for new message ready to published & works on publishing
 // it to all topic subscribers
-func (h *Hub) Process(ctx context.Context, running chan struct{}) {
+func (h *Hub) process(ctx context.Context, running chan struct{}) {
 	close(running)
 
 	op := ops.MSG_PUSH
@@ -350,67 +372,28 @@ func (h *Hub) Process(ctx context.Context, running chan struct{}) {
 			return
 
 		case <-h.ping:
-			msg := h.Next()
-			h.publish(&op, msg)
+			msg := h.next()
+			h.writeMessage(ctx, &op, msg)
 
 		case <-time.After(time.Duration(100) * time.Millisecond):
 			started := time.Now()
 
-			for msg := h.Next(); msg != nil; {
+			for msg := h.next(); msg != nil; {
 				if time.Since(started) > time.Duration(100)*time.Millisecond {
 					break
 				}
 
-				h.publish(&op, msg)
+				h.writeMessage(ctx, &op, msg)
 			}
 
 		}
 	}
 }
 
-// Listen - Hub listens for TCP connections, accepts those & spawns
-// new go routine for handling each of those
-func (h *Hub) Listen(ctx context.Context, addr string, done chan bool) {
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Printf("[pub0sub] Error : %s\n", err.Error())
-
-		done <- false
-		return
-	}
-
-	defer func() {
-		if err := lis.Close(); err != nil {
-			log.Printf("[pub0sub] Error : %s\n", err.Error())
-		}
-	}()
-
-	done <- true
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-
-		default:
-			conn, err := lis.Accept()
-			if err != nil {
-				log.Printf("[pub0sub] Error : %s\n", err.Error())
-				return
-			}
-
-			func(conn net.Conn) {
-				go h.handleTCPConnection(ctx, conn)
-			}(conn)
-
-		}
-	}
-}
-
-// Evict - As soon as it's determined peer is not anymore
+// As soon as it's determined peer is not anymore
 // connected & it didn't follow graceful tear down ( didn't unsubscribe from topics )
 // its entry from subscription table to be evicted
-func (h *Hub) Evict(ctx context.Context, running chan struct{}) {
+func (h *Hub) evictSubscribers(ctx context.Context, running chan struct{}) {
 	close(running)
 
 	for {
@@ -441,126 +424,13 @@ func (h *Hub) Evict(ctx context.Context, running chan struct{}) {
 	}
 }
 
-// handleTCPConnection - Each publisher, subscriber connection is handled
-// in this method, as seperate go routine
-func (h *Hub) handleTCPConnection(ctx context.Context, conn net.Conn) {
-	var subscriberId uint64
-	defer func() {
-		if err := conn.Close(); err != nil {
-			log.Printf("[pub0sub] Error : %s\n", err.Error())
-		}
-
-		// If peer is indeed one subscriber,
-		// mark it can be evicted
-		if subscriberId != 0 {
-			h.evict <- subscriberId
-		}
-	}()
-
-STOP:
-	for {
-		select {
-		case <-ctx.Done():
-			return
-
-		default:
-			op := new(ops.OP)
-			_, err := op.ReadFrom(conn)
-			if err != nil {
-				break STOP
-			}
-
-			switch *op {
-			case ops.PUB_REQ:
-				msg := new(ops.Msg)
-				if _, err := msg.ReadFrom(conn); err != nil {
-					break STOP
-				}
-
-				subCount := h.Publish(msg)
-				rOp := ops.PUB_RESP
-				if _, err := rOp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-				pResp := ops.CountResponse(subCount)
-				if _, err := pResp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-			case ops.NEW_SUB_REQ:
-				msg := new(ops.NewSubscriptionRequest)
-				if _, err := msg.ReadFrom(conn); err != nil {
-					break STOP
-				}
-
-				subId, topicCount := h.Subscribe(conn, msg.Topics...)
-				{
-					// keep track of subscriber id,
-					// if peer is indeed subscriber
-					subscriberId = subId
-				}
-				rOp := ops.NEW_SUB_RESP
-				if _, err := rOp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-				sResp := ops.NewSubResponse{Id: subId, TopicCount: topicCount}
-				if _, err := sResp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-			case ops.ADD_SUB_REQ:
-				msg := new(ops.AddSubscriptionRequest)
-				if _, err := msg.ReadFrom(conn); err != nil {
-					break STOP
-				}
-
-				topicCount := h.AddSubscription(msg.Id, conn, msg.Topics...)
-				rOp := ops.ADD_SUB_RESP
-				if _, err := rOp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-				pResp := ops.CountResponse(topicCount)
-				if _, err := pResp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-			case ops.UNSUB_REQ:
-				msg := new(ops.UnsubcriptionRequest)
-				if _, err := msg.ReadFrom(conn); err != nil {
-					break STOP
-				}
-
-				topicCount := h.Unsubscribe(msg.Id, msg.Topics...)
-				rOp := ops.UNSUB_RESP
-				if _, err := rOp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-				pResp := ops.CountResponse(topicCount)
-				if _, err := pResp.WriteTo(conn); err != nil {
-					break STOP
-				}
-
-			case ops.UNSUPPORTED:
-				break STOP
-
-			}
-
-		}
-	}
-
-}
-
 // nextId - Generates next subscriber id [ concurrrent-safe ]
 func (h *Hub) nextId() uint64 {
 	return atomic.AddUint64(&h.index, 1)
 }
 
 // queued - Manager to check whether it has anything to act on
-func (h *Hub) Queued() bool {
+func (h *Hub) queued() bool {
 	h.queueLock.RLock()
 	defer h.queueLock.RUnlock()
 
@@ -568,7 +438,7 @@ func (h *Hub) Queued() bool {
 }
 
 // next - Next queued message to act on, if any
-func (h *Hub) Next() *ops.Msg {
+func (h *Hub) next() *ops.Msg {
 	h.queueLock.Lock()
 	defer h.queueLock.Unlock()
 
@@ -650,10 +520,10 @@ func (h *Hub) topicSubscribe(subId uint64, conn net.Conn, topics ...string) uint
 	return count
 }
 
-// Subscribe - Client sends subscription request with a non-empty list
+// subscribe - Client sends subscription request with a non-empty list
 // of topics it's interested in, for very first time, which is why
 // one unique id to be generated
-func (h *Hub) Subscribe(conn net.Conn, topics ...string) (uint64, uint32) {
+func (h *Hub) subscribe(conn net.Conn, topics ...string) (uint64, uint32) {
 	if len(topics) == 0 {
 		return 0, 0
 	}
@@ -662,9 +532,9 @@ func (h *Hub) Subscribe(conn net.Conn, topics ...string) (uint64, uint32) {
 	return id, h.topicSubscribe(id, conn, topics...)
 }
 
-// AddSubscription - Subscriber showing intent of receiving messages
+// addSubscription - Subscriber showing intent of receiving messages
 // from a non-empty set of topics [ on-the-fly i.e. after subscriber has been registered ]
-func (h *Hub) AddSubscription(subId uint64, conn net.Conn, topics ...string) uint32 {
+func (h *Hub) addSubscription(subId uint64, conn net.Conn, topics ...string) uint32 {
 	if len(topics) == 0 {
 		return 0
 	}
@@ -674,7 +544,7 @@ func (h *Hub) AddSubscription(subId uint64, conn net.Conn, topics ...string) uin
 
 // Unsubscribe - Subscriber shows intent of not receiving messages
 // from non-empty set of topics
-func (h *Hub) Unsubscribe(subId uint64, topics ...string) uint32 {
+func (h *Hub) unsubscribe(subId uint64, topics ...string) uint32 {
 	if len(topics) == 0 {
 		return 0
 	}
@@ -702,11 +572,11 @@ func (h *Hub) Unsubscribe(subId uint64, topics ...string) uint32 {
 	return count
 }
 
-// Publish - Message publish request to be enqueued
+// publish - Message publish request to be enqueued
 // for some worker to process, while this function will
 // calcalate how many clients will receive this message
 // & respond back
-func (h *Hub) Publish(msg *ops.Msg) uint32 {
+func (h *Hub) publish(msg *ops.Msg) uint32 {
 	if len(msg.Topics) == 0 {
 		return 0
 	}
