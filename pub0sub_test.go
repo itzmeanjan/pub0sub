9acba53554a053c05e54fdf8fc18b73a97b0b382
Anjan Roy <anjanroy@yandex.com>
2021-05-25 07:45:54 +0530

    benchmarking

--- pub0sub_test.go
+++ pub0sub_test.go
@@ -5,6 +5,7 @@ package pub0sub
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -93,4 +94,77 @@ func parallelConnection(t *testing.T, count uint64) {
 	cancel()
 	<-time.After(delay)
 	<-consumed
+}
+
+func BenchmarkPublisher4Topics(b *testing.B) {
+	benchmarkPublisher(b, 4)
+}
+
+func BenchmarkPublisher32Topics(b *testing.B) {
+	benchmarkPublisher(b, 32)
+}
+
+func BenchmarkPublisher255Topics(b *testing.B) {
+	benchmarkPublisher(b, 255)
+}
+
+func generateTopics(count uint64) ([]string, uint64) {
+	topics := make([]string, count)
+	var bLen uint64
+
+	for i := 0; i < int(count); i++ {
+		topic := fmt.Sprintf("topic_%d", i+1)
+		topics[i] = topic
+		bLen += uint64(1 + len(topic))
+	}
+
+	return topics, bLen
+}
+
+func benchmarkPublisher(b *testing.B, topicC uint64) {
+	addr := "127.0.0.1:0"
+	proto := "tcp"
+	capacity := uint64(16)
+	topics, bLen := generateTopics(topicC)
+	data := []byte("hello")
+	msg := ops.Msg{Topics: topics, Data: data}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func() {
+		cancel()
+		<-time.After(time.Duration(100) * time.Millisecond)
+	}()
+
+	hub, err := hub.New(ctx, addr, capacity)
+	if err != nil {
+		b.Fatalf("Failed to start Hub : %s\n", err.Error())
+	}
+
+	sub, err := subscriber.New(ctx, proto, hub.Addr(), capacity, topics...)
+	if err != nil {
+		b.Fatalf("Failed to start subscriber : %s\n", err.Error())
+	}
+	defer func() {
+		sub.UnsubscribeAll()
+		sub.Disconnect()
+	}()
+
+	pub, err := publisher.New(ctx, proto, hub.Addr())
+	if err != nil {
+		b.Fatalf("Failed to start publisher : %s\n", err.Error())
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(int64(15 + bLen))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		n, err := pub.Publish(&msg)
+		if err != nil {
+			b.Fatalf("Failed to publish : %s\n", err.Error())
+		}
+		if n != topicC {
+			b.Fatalf("Expected to publish to %d subscribers, did to %d\n", topicC, n)
+		}
+	}
 }
